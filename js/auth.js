16bc9fb9eba3b28ef69523c410d19a78cd44528c
Asmita <asmita.baghsavar.csiot22@ggits.net>
2026-03-16 20:24:11 +0530

    fixed auth

--- js/auth.js
+++ js/auth.js
@@ -1,67 +1,33 @@
 const BASE_URL="https://digital-evidence-backend.onrender.com";
 
-function getToken(){
-return localStorage.getItem("token");
-}
-
-function setToken(token){
-localStorage.setItem("token",token);
-}
-
-function removeToken(){
-localStorage.removeItem("token");
-}
-
-// LOGIN
 async function loginUser(){
 
 const email=document.getElementById("email").value;
 const password=document.getElementById("password").value;
 
 const res=await fetch(`${BASE_URL}/api/auth/login`,{
-
 method:"POST",
-
 headers:{
 "Content-Type":"application/json"
 },
-
 body:JSON.stringify({email,password})
-
 });
 
 const data=await res.json();
 
-if(data.token){
+console.log(data);   // debugging
 
-setToken(data.token);
+if(res.ok){
 
-localStorage.setItem("role",data.role);
+localStorage.setItem("token",data.token);
+localStorage.setItem("role",data.role || data.user?.role);
 
 window.location.href="dashboard.html";
 
 }else{
 
-alert("Login Failed");
+alert(data.message || "Login Failed");
 
 }
 
-}
-
-// LOGOUT
-function logout(){
-
-removeToken();
-
-localStorage.removeItem("role");
-
-window.location.href="index.html";
-
-}
-
-// PAGE PROTECTION
-const page=window.location.pathname.split("/").pop();
-
-if(!getToken() && page!=="index.html" && page!=="otp.html"){
-window.location.href="index.html";
 }
