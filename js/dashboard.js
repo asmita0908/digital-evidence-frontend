eb60783c3fa717603f7a2564699d8f0ba924356f
Asmita <asmita.baghsavar.csiot22@ggits.net>
2026-04-16 21:15:13 +0530

    fixed dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -219,11 +219,16 @@ async function uploadEvidence() {
 
 // ================= PREVIEW =================
 function previewEvidence(url) {
+  if (!url) {
+    alert("No file URL ❌");
+    return;
+  }
+
   let content = "";
 
   if (url.match(/\.(jpg|jpeg|png|gif|webp)/i)) {
     content = `<img src="${url}" width="100%">`;
-  } else if (url.includes(".pdf")) {
+  } else if (url.includes("pdf")) {
     content = `<iframe src="${url}" width="100%" height="500px"></iframe>`;
   } else {
     content = `<a href="${url}" target="_blank">Open File</a>`;
